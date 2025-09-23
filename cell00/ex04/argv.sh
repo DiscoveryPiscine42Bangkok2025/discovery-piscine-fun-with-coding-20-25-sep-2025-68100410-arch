c8eae67a861fdbd229f50b58101e47ed503966d1
68100410-arch <68100410@kmitl.ac.th>
2025-09-23 06:00:56 +0000

    d

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -1,9 +1,9 @@
-# ! /bin /bash
-if [ $# -eq 0 ];then
-    echo " no arguments supplied "
+args=("$@")
+if (($# == 0))
+then
+  echo "No arguments supplied"
 else
-    for arg in "$@"
-    do
-        echo "$arg"
+    for (( i=0;i<3;i++));do
+        echo ${args{${i}}}
     done
 fi
